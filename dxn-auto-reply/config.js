056564bdc3d91f07be82136e43c93e2b4e0fcbb9
abdaalwhab04-lab <abdaalwhab04@gmail.com>
2026-08-06 15:49:40 +0000

    Add chatbot configuration module

---
+++ dxn-auto-reply/config.js
@@ -0,0 +1,13 @@
+const DXN_CONFIG = {
+  whatsapp: "218946098624",
+  sponsorCode: "829134401",
+  registrationLink: "",
+  sheetUrl: ""
+};
+const DXN_CONFIG = {
+  whatsapp: "218946098624",
+  sponsorCode: "829134401",
+  registrationLink: "",
+  sheetUrl: ""
+};
+
